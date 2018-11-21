eab9eb4a68c2018301bd93b431233530aa51db6a
velovsky <ticamara@gmail.com>
2018-11-21 00:39:28 +0000

    first commit

---
+++ src/components/header/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="my-header">
+    <img src="@/assets/ze-logo.png" alt="">
+    <div> SOME LINKS </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myHeader'
+}
+</script>
+
+<style src="./Header.scss" lang="scss" scoped></style>
